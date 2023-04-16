c514acfa1d3c72e6827d3711486c50e7450ca93b
shawntsai0312 <shawntsai0312@gmail.com>
2023-04-16 16:33:10 +0800

    finish

--- backend/src/routes/change.js
+++ backend/src/routes/change.js
@@ -1,8 +1,15 @@
 import express from 'express'
+import { Gpio } from 'onoff'
+
 const router = express.Router()
+var Led = new Gpio(4, 'out')
 
 router.get('/change', (req, res) => {
-    console.log(req.query.state)
-    res.send({ msg: 'receive' })
+    let light = req.query.state === 'true' ? 1 : 0;
+    console.log(light)
+    console.log(Led.readSync())
+    Led.writeSync(light)
+    // if(req.query.state != Led.readSync())
+    light ? res.send({ msg: 'on' }) : res.send({ msg: 'off' })
 })
 export default router
